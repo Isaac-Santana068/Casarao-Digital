14b2f996cdb58e2297430d6b24d472bc5a8128f3
Isaac Santana <santanaisaac068@gmail.com>
2025-11-10 19:27:42 -0300

    beta version 0.0.3
    Atualizações:
    
    *Página home;
    *Screenview;

--- JS/troca_imagem.js
+++ JS/troca_imagem.js
@@ -1,14 +1,12 @@
 const imagens = [
-        "fundo1.jpeg",
-        "assests/fundo2.jpeg"
+        "assests/fundo1.jpeg",
+        "assests/fundo2.jpeg",
+        "assests/fundo3.jpg"
     ]
-
-var indice = 0
-
+let indice = 0
 function trocar_imagem(){
     document.body.style.backgroundImage = `url('${imagens[indice]}')`
-    var indice = (indice + 1) % imagens.length
+    indice = (indice + 1) % imagens.length
 }
-
-trocar_imagens()
-setInterval(5000)
+trocar_imagem()
+setInterval(trocar_imagem, 5000)
